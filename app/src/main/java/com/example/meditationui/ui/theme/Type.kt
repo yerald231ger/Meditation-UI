78deb723f60070b928ba4954f401c662ee5ef491
Gerardo Sanchez Hernandez <yerald231ger@Gerardos-MacBook-Pro.local>
2024-02-11 14:38:46 -0600

    Typography

--- app/src/main/java/com/example/meditationui/ui/theme/Type.kt
+++ app/src/main/java/com/example/meditationui/ui/theme/Type.kt
@@ -2,33 +2,39 @@ package com.example.meditationui.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.meditationui.R
 
-// Set of Material typography styles to start with
+val gothicA1 = FontFamily(
+    listOf(
+        Font(R.font.gothica1_regular, FontWeight.Normal),
+        Font(R.font.gothica1_medium, FontWeight.Medium),
+        Font(R.font.gothica1_semibold, FontWeight.SemiBold),
+        Font(R.font.gothica1_bold, FontWeight.Bold),
+        Font(R.font.gothica1_black, FontWeight.Black),
+    )
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodyMedium = TextStyle(
+        color = AquaBlue,
+        fontFamily = gothicA1,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    headlineLarge = TextStyle(
+        color = TextWhite,
+        fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+    headlineSmall = TextStyle(
+        color = TextWhite,
+        fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
     )
-    */
 )
